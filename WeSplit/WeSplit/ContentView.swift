29b6c57bb0dc5294291ebb3446b41dc363f5e3b3
adprietodev <adrianwork95@outlook.com>
2024-06-10 18:07:10 +0200

    Create first proyect to SwiftUI WeSplit

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Adrian Prieto Villena on 10/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let students = ["Adrian","Miguel","Ruben","Isabel"]
+    @State private var selectedStudent = "Adrian"
+
+    var body: some View {
+        Text("Hello, word")
+    }
+}
+
+#Preview {
+    ContentView()
+}
